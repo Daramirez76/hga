2f7a1ae47f4d65f289e16239a79b4d0825c81b70
JAVI-TRRS <carlosjaviert327@gmail.com>
2026-03-03 21:26:20 +0000

    se estaba arreglo problemas basicos

--- db/hogar_geriatrico.sql
+++ db/hogar_geriatrico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-08-2025 a las 05:46:26
+-- Tiempo de generación: 02-03-2026 a las 23:46:42
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -18,14 +18,14 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `hogar_geriatrico_`
+-- Base de datos: `hga`
 --
 
 DELIMITER $$
 --
 -- Procedimientos
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_agregar_medicamento` (IN `p_nombre_medic` VARCHAR(10), IN `p_fecha_entrada` DATE, IN `p_fecha_vencimiento` DATE, IN `p_cod_usuario` INT, IN `p_cod_residente` INT, IN `p_cod_rol` INT, IN `p_descrip_novedad` VARCHAR(100), IN `p_fecha_novedad` DATE, IN `p_disponibilidad` INT)   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_agregar_medicamento` (IN `p_nombre_medic` VARCHAR(10), IN `p_fecha_entrada` DATE, IN `p_fecha_vencimiento` DATE, IN `p_cod_usuario` INT, IN `p_cod_residente` INT, IN `p_cod_rol` INT, IN `p_descrip_novedad` VARCHAR(100), IN `p_fecha_novedad` DATE, IN `p_stock` INT)   BEGIN
     INSERT INTO medicamentos (nombre_medic, fecha_entrada, fecha_vencimiento, cod_usuario, cod_residente, cod_rol, descrip_novedad, fecha_novedad, disponibilidad)
     VALUES (p_nombre_medic, p_fecha_entrada, p_fecha_vencimiento, p_cod_usuario, p_cod_residente, p_cod_rol, p_descrip_novedad, p_fecha_novedad, p_disponibilidad);
 END$$
@@ -35,7 +35,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_agregar_residente` (IN `p_nombre
     VALUES (p_nombre, p_apellido, p_edad, p_patologia, p_RH, p_cod_usuario, p_cod_rol);
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_agregar_usuario` (IN `p_tipo_doc` VARCHAR(16), IN `p_doc_id` INT, IN `p_nombre` VARCHAR(100), IN `p_apellido` VARCHAR(100), IN `p_direccion` VARCHAR(150), IN `p_telefono` BIGINT, IN `p_email` VARCHAR(100), IN `p_usuario` VARCHAR(100), IN `p_contraseña` VARCHAR(32), IN `p_cod_rol` INT, IN `p_parentesco` VARCHAR(32))   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_agregar_usuario` (IN `p_tipo_doc` VARCHAR(16), IN `p_doc_id` INT, IN `p_nombre` VARCHAR(100), IN `p_apellido` VARCHAR(100), IN `p-edad` INT(3), IN `p_direccion` VARCHAR(150), IN `p_telefono` BIGINT, IN `p_email` VARCHAR(100), IN `p_usuario` VARCHAR(100), IN `p_contraseña` VARCHAR(32), IN `p_cod_rol` INT, IN `p_parentesco` VARCHAR(32))   BEGIN
     INSERT INTO usuario (tipo_doc, doc_id, nombre, apellido, direccion, telefono, email, usuario, contraseña, cod_rol, parentesco)
     VALUES (p_tipo_doc, p_doc_id, p_nombre, p_apellido, p_direccion, p_telefono, p_email, p_usuario, p_contraseña, p_cod_rol, p_parentesco);
 END$$
@@ -55,6 +55,7 @@ CREATE TABLE `actividades_ludicas` (
   `Hora_ini` time NOT NULL,
   `Hora_fin` time NOT NULL,
   `cod_residente` int(10) NOT NULL,
+  `cod_rol` int(10) NOT NULL,
   `Lugar` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -62,12 +63,12 @@ CREATE TABLE `actividades_ludicas` (
 -- Volcado de datos para la tabla `actividades_ludicas`
 --
 
-INSERT INTO `actividades_ludicas` (`Cod_acti_ludi`, `Nombre`, `Fecha`, `Hora_ini`, `Hora_fin`, `cod_residente`, `Lugar`) VALUES
-(1, 'Actividad física ', '2025-09-14', '10:00:00', '11:00:00', 5, 'Hogar Geriátrico Años Dorados'),
-(2, 'Pintura', '2025-09-10', '09:30:00', '10:20:00', 2, 'Hogar Geriátrico Años Dorados'),
-(3, 'Actividad de memoria', '2025-09-11', '15:00:00', '15:45:00', 4, 'Hogar Geriátrico Años Dorados'),
-(4, 'Danzas', '2025-09-19', '10:15:00', '11:00:00', 3, 'Hogar Geriátrico Años Dorados'),
-(5, 'Fisioterapia', '2025-09-22', '09:00:00', '10:30:00', 1, 'Hogar Geriátrico Años Dorados');
+INSERT INTO `actividades_ludicas` (`Cod_acti_ludi`, `Nombre`, `Fecha`, `Hora_ini`, `Hora_fin`, `cod_residente`, `cod_rol`, `Lugar`) VALUES
+(1, 'Actividad física ', '2025-09-14', '10:00:00', '11:00:00', 5, 2, 'Hogar Geriátrico Años Dorados'),
+(2, 'Pintura', '2025-09-10', '09:30:00', '10:20:00', 2, 2, 'Hogar Geriátrico Años Dorados'),
+(3, 'Actividad de memoria', '2025-09-11', '15:00:00', '15:45:00', 4, 2, 'Hogar Geriátrico Años Dorados'),
+(4, 'Danzas', '2025-09-19', '10:15:00', '11:00:00', 3, 2, 'Hogar Geriátrico Años Dorados'),
+(5, 'Fisioterapia', '2025-09-22', '09:00:00', '10:30:00', 1, 2, 'Hogar Geriátrico Años Dorados');
 
 -- --------------------------------------------------------
 
@@ -101,7 +102,7 @@ INSERT INTO `citas` (`cod_cita`, `Fecha_cita`, `hora_inicio`, `hora_fin`, `Nombr
 --
 DELIMITER $$
 CREATE TRIGGER `tr_notificacion_cita` AFTER INSERT ON `citas` FOR EACH ROW BEGIN
-    INSERT INTO notificaciones (cod_usuario, Cod_rol, cod_Residente, Descrip_Novedad)
+    INSERT INTO notificaciones (cod_usuario, cod_Residente, Descrip_Novedad)
    VALUES ( NEW.hora_inicio, NEW.Lugar_cita, NEW.Nombre_acompañante); 
    
 END
@@ -136,29 +137,6 @@ INSERT INTO `informes` (`cod_Informes`, `doc_id`, `cod_Residente`, `Titulo_Infor
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `inventario`
---
-
-CREATE TABLE `inventario` (
-  `Cod_inventario` int(10) NOT NULL,
-  `Disponibilidad` tinyint(1) NOT NULL,
-  `Cod_medicamento` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `inventario`
---
-
-INSERT INTO `inventario` (`Cod_inventario`, `Disponibilidad`, `Cod_medicamento`) VALUES
-(1, 1, 4),
-(2, 0, 5),
-(3, 0, 1),
-(4, 1, 2),
-(5, 1, 3);
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `medicamentos`
 --
 
@@ -172,14 +150,14 @@ CREATE TABLE `medicamentos` (
   `cod_rol` int(10) NOT NULL,
   `descrip_novedad` varchar(100) NOT NULL,
   `fecha_novedad` date NOT NULL,
-  `disponibilidad` int(150) NOT NULL
+  `stock` int(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `medicamentos`
 --
 
-INSERT INTO `medicamentos` (`Cod_medicamento`, `nombre_medic`, `fecha_entrada`, `fecha_vencimiento`, `cod_usuario`, `cod_residente`, `cod_rol`, `descrip_novedad`, `fecha_novedad`, `disponibilidad`) VALUES
+INSERT INTO `medicamentos` (`Cod_medicamento`, `nombre_medic`, `fecha_entrada`, `fecha_vencimiento`, `cod_usuario`, `cod_residente`, `cod_rol`, `descrip_novedad`, `fecha_novedad`, `stock`) VALUES
 (1, 'Aspirina', '2025-08-01', '2027-08-01', 30098765, 1, 6, 'Entrega inicial de medicamento', '2025-08-01', 50),
 (2, 'Insulina', '2025-07-15', '2026-07-15', 30098765, 2, 6, 'Tratamiento de diabetes tipo 2', '2025-07-16', 30),
 (3, 'Paracetam', '2025-06-20', '2027-06-20', 30098765, 3, 6, 'Uso en caso de fiebre y dolor', '2025-06-21', 100),
@@ -195,7 +173,6 @@ INSERT INTO `medicamentos` (`Cod_medicamento`, `nombre_medic`, `fecha_entrada`, 
 CREATE TABLE `notificaciones` (
   `cod_Notificaciones` int(10) NOT NULL,
   `cod_usuario` int(10) NOT NULL,
-  `Cod_rol` int(10) NOT NULL,
   `cod_Residente` int(10) NOT NULL,
   `Descrip_Novedad` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -204,12 +181,12 @@ CREATE TABLE `notificaciones` (
 -- Volcado de datos para la tabla `notificaciones`
 --
 
-INSERT INTO `notificaciones` (`cod_Notificaciones`, `cod_usuario`, `Cod_rol`, `cod_Residente`, `Descrip_Novedad`) VALUES
-(1, 123456, 10, 1, 'El residente de Nombre Alejandro debe de tomarse las\r\nAspirinas todos los días a las 10'),
-(2, 10012345, 10, 2, 'El residente de nombre Sebastián tiene reunión el 2025-09-02'),
-(3, 10067890, 10, 4, 'El residente de Nombre Valentina tiene una visita cancelada'),
-(4, 1022937343, 10, 5, 'El residente de Nombre Andrés tiene una cita médica agendada '),
-(5, 30098765, 10, 3, 'El residente de nombre Sebastián tiene una visita Pendiente');
+INSERT INTO `notificaciones` (`cod_Notificaciones`, `cod_usuario`, `cod_Residente`, `Descrip_Novedad`) VALUES
+(1, 123456, 1, 'El residente de Nombre Alejandro debe de tomarse las\r\nAspirinas todos los días a las 10'),
+(2, 10012345, 2, 'El residente de nombre Sebastián tiene reunión el 2025-09-02'),
+(3, 10067890, 4, 'El residente de Nombre Valentina tiene una visita cancelada'),
+(4, 1022937343, 5, 'El residente de Nombre Andrés tiene una cita médica agendada '),
+(5, 30098765, 3, 'El residente de nombre Sebastián tiene una visita Pendiente');
 
 -- --------------------------------------------------------
 
@@ -250,7 +227,6 @@ CREATE TABLE `reuniones` (
   `Link_Videollamada` varchar(30) NOT NULL,
   `cod_usuario` int(10) NOT NULL,
   `cod_Residente` int(10) NOT NULL,
-  `Cod_rol` int(10) NOT NULL,
   `Fecha_Reunion` date NOT NULL,
   `Disponibilidad_dispoditivos` varchar(30) NOT NULL,
   `Confirmacion_personal` varchar(30) NOT NULL
@@ -260,12 +236,12 @@ CREATE TABLE `reuniones` (
 -- Volcado de datos para la tabla `reuniones`
 --
 
-INSERT INTO `reuniones` (`cod_Reunion`, `Link_Videollamada`, `cod_usuario`, `cod_Residente`, `Cod_rol`, `Fecha_Reunion`, `Disponibilidad_dispoditivos`, `Confirmacion_personal`) VALUES
-(1, 'https://fakevideocall.com', 123456, 1, 11, '2025-08-30', 'Limitados', 'pendiente'),
-(2, 'https://fakevideocall2.com', 10012345, 1, 11, '2025-08-31', 'Limitados', 'Pendiente'),
-(3, 'https://fakevideocall3.com', 10067890, 3, 11, '2025-09-02', 'Limitados', 'Pendiente'),
-(4, 'https://fakevideocall4.com', 107524976, 4, 11, '2025-09-16', 'Limitados', 'Pendiente'),
-(5, 'https://fakevideocall5.com', 30098765, 5, 11, '2025-08-13', 'Limitados', 'Aceptados');
+INSERT INTO `reuniones` (`cod_Reunion`, `Link_Videollamada`, `cod_usuario`, `cod_Residente`, `Fecha_Reunion`, `Disponibilidad_dispoditivos`, `Confirmacion_personal`) VALUES
+(1, 'https://fakevideocall.com', 123456, 1, '2025-08-30', 'Limitados', 'pendiente'),
+(2, 'https://fakevideocall2.com', 10012345, 1, '2025-08-31', 'Limitados', 'Pendiente'),
+(3, 'https://fakevideocall3.com', 10067890, 3, '2025-09-02', 'Limitados', 'Pendiente'),
+(4, 'https://fakevideocall4.com', 107524976, 4, '2025-09-16', 'Limitados', 'Pendiente'),
+(5, 'https://fakevideocall5.com', 30098765, 5, '2025-08-13', 'Limitados', 'Aceptados');
 
 -- --------------------------------------------------------
 
@@ -284,7 +260,7 @@ CREATE TABLE `roles` (
 
 INSERT INTO `roles` (`cod_rol`, `nombre_rol`) VALUES
 (1, 'administrador'),
-(2, 'cuidador'),
+(2, 'enfermero'),
 (3, 'residente'),
 (4, 'tutor'),
 (5, 'familiar'),
@@ -308,6 +284,7 @@ CREATE TABLE `usuario` (
   `doc_id` int(10) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `apellido` varchar(100) NOT NULL,
+  `edad` int(3) NOT NULL,
   `direccion` varchar(150) NOT NULL,
   `telefono` bigint(15) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -321,13 +298,13 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`tipo_doc`, `doc_id`, `nombre`, `apellido`, `direccion`, `telefono`, `email`, `usuario`, `contraseña`, `cod_rol`, `parentesco`) VALUES
-('cc', 123456, 'Laura', 'Gómez', 'Calle 50', 3112221111, 'laura@gmail.com', 'lgomez', 'clave123', 2, ''),
-('cc', 10012345, 'juan', 'perez', 'calle 10 #15-20', 3004567890, 'juan.perez@gmail.com', 'jperez', 'passJuan1', 1, ''),
-('cc', 10067890, 'carlos', 'rodriguez', 'av siempre viva 742', 3026543210, 'carlos.rodr@gmail.com', 'crodriguez', 'car1os', 4, ''),
-('ce', 30098765, 'ana', 'martinez', 'calle 50 #10-12', 3112223344, 'ana.mtz@gmail.com', 'amartinez', 'mari1234', 2, ''),
-('cc', 107524976, 'mariana', 'martinez', 'av chile 142', 3134147533, 'marian125@gmail.com', 'mariani', 'martinez125', 4, ''),
-('cc', 1022937343, 'sandra', 'jimenez', 'cra 5 #78-15', 3113156888, 'sandra22@gmail.com', 'sandrita', 'sanmenez3', 4, '');
+INSERT INTO `usuario` (`tipo_doc`, `doc_id`, `nombre`, `apellido`, `edad`, `direccion`, `telefono`, `email`, `usuario`, `contraseña`, `cod_rol`, `parentesco`) VALUES
+('cc', 123456, 'Laura', 'Gómez', 25, 'Calle 50', 3112221111, 'laura@gmail.com', 'lgomez', 'clave123', 2, ''),
+('cc', 10012345, 'juan', 'perez', 30, 'calle 10 #15-20', 3004567890, 'juan.perez@gmail.com', 'jperez', 'passJuan1', 1, ''),
+('cc', 10067890, 'carlos', 'rodriguez', 22, 'av siempre viva 742', 3026543210, 'carlos.rodr@gmail.com', 'crodriguez', 'car1os', 4, ''),
+('ce', 30098765, 'ana', 'martinez', 35, 'calle 50 #10-12', 3112223344, 'ana.mtz@gmail.com', 'amartinez', 'mari1234', 2, ''),
+('cc', 107524976, 'mariana', 'martinez', 28, 'av chile 142', 3134147533, 'marian125@gmail.com', 'mariani', 'martinez125', 4, ''),
+('cc', 1022937343, 'sandra', 'jimenez', 27, 'cra 5 #78-15', 3113156888, 'sandra22@gmail.com', 'sandrita', 'sanmenez3', 4, '');
 
 -- --------------------------------------------------------
 
@@ -367,7 +344,7 @@ CREATE TABLE `vista_medicamentos` (
 ,`nombre_medic` varchar(10)
 ,`fecha_entrada` date
 ,`fecha_vencimiento` date
-,`disponibilidad` int(150)
+,`stock` int(150)
 ,`descrip_novedad` varchar(100)
 ,`fecha_novedad` date
 ,`cod_residente` int(10)
@@ -421,7 +398,7 @@ CREATE TABLE `vista_usuarios` (
 --
 DROP TABLE IF EXISTS `vista_medicamentos`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vista_medicamentos`  AS SELECT `m`.`Cod_medicamento` AS `Cod_medicamento`, `m`.`nombre_medic` AS `nombre_medic`, `m`.`fecha_entrada` AS `fecha_entrada`, `m`.`fecha_vencimiento` AS `fecha_vencimiento`, `m`.`disponibilidad` AS `disponibilidad`, `m`.`descrip_novedad` AS `descrip_novedad`, `m`.`fecha_novedad` AS `fecha_novedad`, `res`.`cod_residente` AS `cod_residente`, `res`.`nombre` AS `nombre_residente`, `res`.`apellido` AS `apellido_residente`, `u`.`doc_id` AS `codigo_usuario`, `u`.`nombre` AS `nombre_usuario`, `u`.`apellido` AS `apellido_usuario`, `r`.`nombre_rol` AS `rol_asignado` FROM (((`medicamentos` `m` join `residente` `res` on(`m`.`cod_residente` = `res`.`cod_residente`)) join `usuario` `u` on(`m`.`cod_usuario` = `u`.`doc_id`)) join `roles` `r` on(`m`.`cod_rol` = `r`.`cod_rol`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vista_medicamentos`  AS SELECT `m`.`Cod_medicamento` AS `Cod_medicamento`, `m`.`nombre_medic` AS `nombre_medic`, `m`.`fecha_entrada` AS `fecha_entrada`, `m`.`fecha_vencimiento` AS `fecha_vencimiento`, `m`.`stock` AS `stock`, `m`.`descrip_novedad` AS `descrip_novedad`, `m`.`fecha_novedad` AS `fecha_novedad`, `res`.`cod_residente` AS `cod_residente`, `res`.`nombre` AS `nombre_residente`, `res`.`apellido` AS `apellido_residente`, `u`.`doc_id` AS `codigo_usuario`, `u`.`nombre` AS `nombre_usuario`, `u`.`apellido` AS `apellido_usuario`, `r`.`nombre_rol` AS `rol_asignado` FROM (((`medicamentos` `m` join `residente` `res` on(`m`.`cod_residente` = `res`.`cod_residente`)) join `usuario` `u` on(`m`.`cod_usuario` = `u`.`doc_id`)) join `roles` `r` on(`m`.`cod_rol` = `r`.`cod_rol`)) ;
 
 -- --------------------------------------------------------
 
@@ -450,7 +427,8 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 ALTER TABLE `actividades_ludicas`
   ADD PRIMARY KEY (`Cod_acti_ludi`),
-  ADD KEY `cod_residente` (`cod_residente`);
+  ADD KEY `cod_residente` (`cod_residente`),
+  ADD KEY `actividades_ludicas_ibfk_2` (`cod_rol`);
 
 --
 -- Indices de la tabla `citas`
@@ -469,13 +447,6 @@ ALTER TABLE `informes`
   ADD KEY `cod_rol` (`cod_rol`);
 
 --
--- Indices de la tabla `inventario`
---
-ALTER TABLE `inventario`
-  ADD PRIMARY KEY (`Cod_inventario`),
-  ADD KEY `inventario` (`Cod_medicamento`);
-
---
 -- Indices de la tabla `medicamentos`
 --
 ALTER TABLE `medicamentos`
@@ -490,8 +461,7 @@ ALTER TABLE `medicamentos`
 ALTER TABLE `notificaciones`
   ADD PRIMARY KEY (`cod_Notificaciones`),
   ADD KEY `Id_usuario` (`cod_usuario`),
-  ADD KEY `Id_Residente` (`cod_Residente`),
-  ADD KEY `Cod_rol` (`Cod_rol`);
+  ADD KEY `Id_Residente` (`cod_Residente`);
 
 --
 -- Indices de la tabla `residente`
@@ -507,8 +477,7 @@ ALTER TABLE `residente`
 ALTER TABLE `reuniones`
   ADD PRIMARY KEY (`cod_Reunion`),
   ADD KEY `Id_usuario` (`cod_usuario`),
-  ADD KEY `Id_Residente` (`cod_Residente`),
-  ADD KEY `Cod_rol` (`Cod_rol`);
+  ADD KEY `Id_Residente` (`cod_Residente`);
 
 --
 -- Indices de la tabla `roles`
@@ -541,7 +510,8 @@ ALTER TABLE `visitas`
 -- Filtros para la tabla `actividades_ludicas`
 --
 ALTER TABLE `actividades_ludicas`
-  ADD CONSTRAINT `actividades_ludicas_ibfk_1` FOREIGN KEY (`cod_residente`) REFERENCES `residente` (`cod_residente`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `actividades_ludicas_ibfk_1` FOREIGN KEY (`cod_residente`) REFERENCES `residente` (`cod_residente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `actividades_ludicas_ibfk_2` FOREIGN KEY (`cod_rol`) REFERENCES `roles` (`cod_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `citas`
@@ -558,12 +528,6 @@ ALTER TABLE `informes`
   ADD CONSTRAINT `informes_ibfk_3` FOREIGN KEY (`cod_rol`) REFERENCES `roles` (`cod_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `inventario`
---
-ALTER TABLE `inventario`
-  ADD CONSTRAINT `inventario` FOREIGN KEY (`Cod_medicamento`) REFERENCES `medicamentos` (`Cod_medicamento`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Filtros para la tabla `medicamentos`
 --
 ALTER TABLE `medicamentos`
@@ -576,8 +540,7 @@ ALTER TABLE `medicamentos`
 --
 ALTER TABLE `notificaciones`
   ADD CONSTRAINT `notificaciones_ibfk_1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`doc_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `notificaciones_ibfk_2` FOREIGN KEY (`cod_Residente`) REFERENCES `residente` (`cod_residente`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `notificaciones_ibfk_3` FOREIGN KEY (`Cod_rol`) REFERENCES `roles` (`cod_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `notificaciones_ibfk_2` FOREIGN KEY (`cod_Residente`) REFERENCES `residente` (`cod_residente`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `residente`
@@ -591,8 +554,7 @@ ALTER TABLE `residente`
 --
 ALTER TABLE `reuniones`
   ADD CONSTRAINT `reuniones_ibfk_1` FOREIGN KEY (`cod_usuario`) REFERENCES `usuario` (`doc_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reuniones_ibfk_2` FOREIGN KEY (`cod_Residente`) REFERENCES `residente` (`cod_residente`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reuniones_ibfk_3` FOREIGN KEY (`Cod_rol`) REFERENCES `roles` (`cod_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `reuniones_ibfk_2` FOREIGN KEY (`cod_Residente`) REFERENCES `residente` (`cod_residente`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `usuario`
