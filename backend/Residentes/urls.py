0966a8cc8c3f58c4b4b9d5b34265213fdb3f23c0
JAVI-TRRS <carlosjaviert327@gmail.com>
2026-03-03 22:23:55 +0000

    se creo la appi de residentes

--- backend/Residentes/urls.py
+++ backend/Residentes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ResidenteViewSet
 
 urlpatterns = [
-    path("", ResidenteViewSet.as_view({"get": "list", "post": "create"}), name="residente-list"),
-    path("<int:pk>/", ResidenteViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"}), name="residente-detail"),
+    path("Residentes/", ResidenteViewSet.as_view({"get": "list", "post": "create"}), name="residente-list"),
+    path("Residentes/<int:pk>/", ResidenteViewSet.as_view({"get": "retrieve", "put": "update", "delete": "destroy"}), name="residente-detail"),
 ]
